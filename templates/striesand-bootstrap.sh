ea3836e167428c7f105543500ca19c385d82e04c
Alan Hollis <me@alanhollis.com>
2018-01-22 20:38:41 +0000

    Fix user data

--- templates/striesand-bootstrap.sh
+++ templates/striesand-bootstrap.sh
@@ -4,8 +4,7 @@ sudo apt install -y git python-pip
 sudo pip install --no-input ansible markupsafe
 source ~/.bashrc
 cd /home/ubuntu/
-mkdir /home/ubuntu/.ssh/
-ssh-keygen -t rsa -N "" -f /home/ubuntu/.ssh/id_rsa
+rm -f /home/ubuntu/.ssh/id_rsa && ssh-keygen -t rsa -N "" -f /home/ubuntu/.ssh/id_rsa
 git clone https://github.com/StreisandEffect/streisand.git && cd streisand
 sudo tee global_vars/noninteractive/local-site.yml <<EOF
 ${local_site}
@@ -14,13 +13,12 @@ deploy/streisand-local.sh --site-config global_vars/noninteractive/local-site.ym
 sudo mkdir -p /etc/openvpn/ccd
 
 sed -i '/^compress/d' /etc/openvpn/server.conf
-sudo tee /etc/openvpn/server.conf <<EOF
-    push "route 10.8.0.0 255.255.255.0"
-    client-config-dir ccd
-    route 192.168.4.0. 255.255.255.0
-    duplicate-cn
+sed -i '/^push \"red/d' /etc/openvpn/server.conf
+sudo tee -a /etc/openvpn/server.conf <<-EOF
     compress lz4-v2
     push "compress lz4-v2"
+    client-config-dir ccd
+    duplicate-cn
 EOF
 
 sudo service openvpn restart
